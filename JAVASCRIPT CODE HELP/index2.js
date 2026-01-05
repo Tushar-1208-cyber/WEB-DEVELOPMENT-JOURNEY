a7aa1038efd06e3d54d9d5359c4bc3e5a7e128dd
Tushar-1208-cyber <tushargupta20055@gmail.com>
2026-01-05 21:10:01 +0530

    Day 67 - JavaScript logic practice 2

---
+++ JAVASCRIPT CODE HELP/index2.js
@@ -0,0 +1,96 @@
+// console.log("Tushar");
+
+// ARITHEMATIC OPERATORS
+// let a = 10;
+// let b = 5;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+
+
+// ASSIGNMENT OPERATORS
+// let a = 2;
+// let b = 5;
+// a = a+10;
+// console.log(a);
+// a=a-2;
+// console.log(a);
+
+
+// COMPARSION OPERATORS
+// let a = 2;
+// let b = 5;
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a>=b);
+// console.log(a<=b);
+// console.log(a==b);
+// console.log(a===b);
+// console.log(a!=b);
+// console.log(a!==b);
+
+
+// TERNARY OPERATORS
+// let age = 25;
+// let status1 = (age>18) ? 'I can vote' : 'I cannot vote';
+// console.log(status1);
+
+
+// LOGICAL OPERATORS
+// let ans = (true && true && true)
+// console.log(ans);
+// let ans1 = (false && false && false)
+// console.log(ans1);
+// let ans2 =!(false)
+// console.log(ans2);
+// NON - BOOLEAN LOGICAL
+// console.log(false || "Babbar");
+// console.log(false || 7);
+// SHORT CIRCUITING LOGICAL
+// console.log(false || 7 || 11 || 18);
+
+
+// BITWISE OPERATORS
+// console.log(2&5);
+// console.log(2|5);
+// console.log(~0);
+// console.log(2^2);
+// LEFT-SHIFT AND RIGHT-SHIFT
+// console.log(10>>2);
+// console.log(10<<2);
+
+
+
+
+// CONDITIONAL STATEMENTS
+
+// IF-ELSE STATEMENTS
+// let age = 53;
+// if(age >= 18)
+// {
+//     console.log('Can vote');
+// }
+// else
+// {
+//     console.log('Cannot vote');
+// }
+
+// we can write if  else-if else-if and else statements
+
+// SWITCH STATEMENTS
+// let number = 3;
+// switch(number)
+// {
+//     case 1: console.log('A');
+//     break;
+//     case 2: console.log('B');
+//     break;
+//     case 3: console.log('C');
+//     break;
+//     case 4: console.log('D');
+//     break;
+//     default: console.log('F');
+// }
