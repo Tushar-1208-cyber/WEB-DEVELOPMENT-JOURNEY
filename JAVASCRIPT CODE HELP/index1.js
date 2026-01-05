5108b05c0e128a6279d8788cfd89143b5a3a9106
Tushar-1208-cyber <tushargupta20055@gmail.com>
2026-01-05 21:09:41 +0530

    Day 65 - JavaScript logic practice 1

---
+++ JAVASCRIPT CODE HELP/index1.js
@@ -0,0 +1,77 @@
+// var age = 25;
+
+// if(true){
+//     console.log(age);
+// }
+
+
+
+
+
+// function solve(){
+//     var age = 25;
+//     console.log(age);
+// }
+// solve();
+// console.log(age); 
+// it will show error because age is not defined outside function
+
+
+
+// var x=10;
+// var x=20;
+// ye nhi krna hai 
+
+
+
+
+// {
+//     let a = 10;
+//     console.log(a);
+// }
+// isi ke andar access kr paenge only 
+
+
+
+// let a=10;
+// let a=20;
+
+
+// let a = 10;
+// a = "babbar";
+// a = true;
+// a = null;
+
+// const a=28;
+// console.log(a);
+
+// const a=20;
+// console.log(a);
+
+
+// let marks = 20.2345;
+// marks="babbar";
+// marks = true;
+// console.log(marks);
+
+// let marks;
+// console.log(marks);
+
+
+// let marks=null;
+// console.log(marks)
+
+
+let marks = 3897712876967364753645697845132654236759516666732559125477893786876786745675465576567956793546236;
+console.log(marks);
+
+marks=false;
+console.log(typeof(marks));
+marks=12;
+console.log(typeof(marks));
+marks="babbar";
+console.log(typeof(marks));
+marks=null;
+console.log(typeof(marks));
+marks;
+console.log(typeof(marks));
