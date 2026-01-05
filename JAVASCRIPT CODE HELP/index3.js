5fe4e92a66979beaeacc4286215b3b5040ed2992
Tushar-1208-cyber <tushargupta20055@gmail.com>
2026-01-05 21:10:17 +0530

    Day 69 - JavaScript logic practice 3

---
+++ JAVASCRIPT CODE HELP/index3.js
@@ -0,0 +1,188 @@
+// console.log("babbar");
+
+// FOR LOOP
+// for(let i=1;i<=10;i++)
+// {
+//     console.log("babbar");
+// }
+
+// FOR LOOP - COUNTING
+// for(let i=1;i<=5;i++)
+// {
+//     console.log(i);
+// }
+
+// FOR LOOP - REVERSE COUNTING
+// for(let i=5;i>0;i--)
+// {
+//     console.log(i);
+// }
+
+
+// FOR LOOP - BREAK STATEMENT
+// for(let i=1;i<=6;i++)
+// {
+//     if(i==4)
+//     {
+//         break;
+//     }
+//     else
+//     {
+//         console.log(i);
+//     }
+// }
+
+
+// FOR LOOP - CONTINUE STATEMENT
+// for(let i=1;i<=4;i++)
+// {
+//     if(i==3)
+//     {
+//         continue;
+//     }
+//     else
+//     {
+//         console.log(i);
+//     }
+// }
+
+
+// WHILE LOOP
+// let i=1;
+// while(i<=10){
+//     console.log("babbar");
+//     i++;
+// }
+
+
+// WHILE LOOP - COUNTING
+// let i=1;
+// while(i<=5){
+//     console.log(i);
+//     i++;
+// }
+
+
+// WHILE LOOP - REVERSE COUNTING
+// let i=5;
+// while(i>0){
+//     console.log(i);
+//     i--;
+// }
+
+
+// WHILE LOOP - BREAK STATEMENT
+// let i=1;
+// while(i<=5){
+//     if(i==3)
+//     {
+//         break;
+//     }
+//     else
+//     {
+//         console.log(i)
+//     }
+//     i++;
+// }
+
+
+// WHILE LOOP - CONTINUE STATEMENT
+// let i=1;
+// while(i<5){
+//     console.log("Inside the loop")
+//     if(i==3)
+//     {
+//         i++;
+//         continue;
+//     }
+//     else
+//     {
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+
+// let i=1;
+// do{
+//     console.log("babbar");
+//     i++;
+
+// }while(i<=10);
+
+
+// let i=1;
+// do{
+//     console.log(i);
+//     i++;
+
+// }while(i<=5);
+
+
+// let i=5;
+// do{
+//     console.log(i);
+//     i--;
+
+// }while(i>0);
+
+
+
+
+// let firstName = "Love";
+// let lastName = 'Babbar';
+
+// let name = `Love
+// Babbar This is
+// my 
+// name `;
+// console.log(name);
+
+// console.log(typeof(firstName));
+
+// let firstName = new String("Love Babbar");
+// console.log(firstName);
+
+
+// let op1 = 'English ';
+// let op2 = 'Hindi ';
+// let ans = op1 + op2;
+// console.log(ans);
+
+
+
+
+// let op1 = 'English ';
+// let op2 = 'Hindi ';
+// let finalans = `op1 + op2`;
+// let finalans = `${op1} + ${op2}`;
+// console.log(finalans);
+
+
+
+
+// let op1 = 'English ';
+// let op2 = 'HINDI ';
+
+// console.log(op2.length);
+
+// console.log(op1.toUpperCase());
+
+// console.log(op2.toLowerCase());
+
+
+
+
+
+// let str = "Babbar";
+// console.log(str.substring(2));
+// console.log(str.substring(2,4));
+
+
+
+
+// let sentence = "Hello \\Ji \\Kaise \\Ho \\Saare";
+// let words = sentence.split('\\');
+// console.log(words);
+
+// console.log(words.join(','));
